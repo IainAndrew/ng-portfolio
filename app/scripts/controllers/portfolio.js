7b7f1955431936f44188e6292903ed2e0a7315fa
Iain Andrew <iainbruceandrew@gmail.com>
2014-11-10 21:59:09 +0000

    portfolio restyling

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -7,31 +7,36 @@ angular.module('app')
       	'name':'Plugmedia', 
       	'roles':['Web Design', 'Web Development', 'Logo Design', 'Branding'], 
       	'desc':'A website for a Dublin based digital media company Lorem ipsum dolor sit amet, consectetur adipisicing.',
-      	'url':'plugmedia'
+      	'url':'plugmedia',
+        'image':'"../../images/landscape.png'
       },
       {
       	'name':'Peter Ashmore', 
       	'roles':['Web Design', 'Web Development', 'Logo Design', 'Branding'],
       	'desc':'I designed this website for an audio engineer',
-      	'url':'peter-ashmore'
+      	'url':'peter-ashmore',
+        'image':'"../../images/landscape.png'
       },
       {
       	'name':'Trunkys', 
       	'roles':['Graphic Design'],
       	'desc':'Illustrations for a stupid app',
-      	'url':'trunkys'
+      	'url':'trunkys',
+        'image':'"../../images/landscape.png'
       },
       {
         'name':'Footer-reveal.js', 
         'roles':['Web Development'],
         'desc':'a jquery plugin',
-        'url':'footerreveal'
+        'url':'footerreveal',
+        'image':'../../images/landscape.png'
       },
       {
         'name':'Simplespin.css', 
         'roles':['Web Development'],
         'desc':'a css library',
-        'url':'simplespin'
+        'url':'simplespin',
+        'image':'../../images/landscape.png'
       }
     ];
   });
