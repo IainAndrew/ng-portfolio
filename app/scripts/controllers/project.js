393e113a58f5f383605e7e9f8cbd69da55f5318e
Iain Andrew <iainbruceandrew@gmail.com>
2014-11-22 22:04:59 +0000

    some project content

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -3,104 +3,104 @@
 angular.module('app')
   .controller('ProjectCtrl', function ($scope) {
     $scope.projects = [
-      {
-      	'name':'Plugmedia', 
-      	'items':[
-      		{
-      			'label':'Overview',
-      			'url':'plugmedia.overview'
-      		}, 
-      		{
-      			'label':'Web Design',
-      			'url':'plugmedia.web-design'
-      		},
-      		{ 
-      			'label':'Web Development',
-      			'url':'plugmedia.web-development'
-      		},
-      		{ 
-      			'label':'Logo Design',
-      			'url':'plugmedia.logo-design'
-      		}
-            ],
-            'prev':'',
-            'next':'#/peter-ashmore'
-      },
-      {
-      	'name':'Peter Ashmore', 
-      	'items':[
-      		{
-      			'label':'Overview',
-      			'url':'peter-ashmore.overview'
-      		}, 
-      		{
-      			'label':'Web Design',
-      			'url':'peter-ashmore.web-design'
-      		},
-      		{ 
-      			'label':'Web Development',
-      			'url':'peter-ashmore.web-development'
-      		},
-                  {
-                        'label':'Graphic Design',
-                        'url':'peter-ashmore.graphic-design'
-                  },
-      		{ 
-      			'label':'Logo Design',
-      			'url':'peter-ashmore.logo-design'
-      		}
-            ],
-            'prev':'#/plugmedia',
-            'next':'#/trunkys'
-      },
-      {
-      	'name':'Trunkys', 
-      	'items':[
-      		{
-      			'label':'Overview',
-      			'url':'trunkys.overview'
-      		}, 
-      		{
-      			'label':'Graphic Design',
-      			'url':'trunkys.graphic-design'
-      		},
-                  {
-                        'label':'UI Design',
-                        'url':'trunkys.ui-design'
-                  }
-            ],
-            'prev':'#/peter-ashmore',
-            'next':'#/footerreveal'
-      },
-      {
-            'name':'Footer-Reveal.js', 
-            'items':[
-                  {
-                        'label':'Overview',
-                        'url':'footerreveal.overview'
-                  }, 
-                  {
-                        'label':'Web Development',
-                        'url':'footerreveal.web-development'
-                  }
-            ],
-            'prev':'#/trunkys',
-            'next':'#/simplespin'
-      },
-      {
-            'name':'Simplespin.css', 
-            'items':[
-                  {
-                        'label':'Overview',
-                        'url':'simplespin.overview'
-                  }, 
-                  {
-                        'label':'Web Development',
-                        'url':'simplespin.web-development'
-                  }
-            ],
-            'prev':'#/footerreveal',
-            'next':''
-      }
+			{
+				'name':'Plugmedia', 
+				'items':[
+					{
+						'label':'Overview',
+						'url':'plugmedia.overview'
+					}, 
+					{
+						'label':'Web Design',
+						'url':'plugmedia.web-design'
+					},
+					{ 
+						'label':'Web Development',
+						'url':'plugmedia.web-development'
+					},
+					{ 
+						'label':'Logo Design',
+						'url':'plugmedia.logo-design'
+					}
+				],
+				'prev':'',
+				'next':'#/peter-ashmore'
+			},
+			{
+				'name':'Peter Ashmore', 
+				'items':[
+					{
+						'label':'Overview',
+						'url':'peter-ashmore.overview'
+					}, 
+					{
+						'label':'Web Design',
+						'url':'peter-ashmore.web-design'
+					},
+					{ 
+						'label':'Web Development',
+						'url':'peter-ashmore.web-development'
+					},
+					{
+						'label':'Graphic Design',
+						'url':'peter-ashmore.graphic-design'
+					},
+					{ 
+						'label':'Logo Design',
+						'url':'peter-ashmore.logo-design'
+					}
+				],
+				'prev':'#/plugmedia',
+				'next':'#/trunkys'
+			},
+			{
+				'name':'Trunkys', 
+				'items':[
+					{
+						'label':'Overview',
+						'url':'trunkys.overview'
+					}, 
+					{
+						'label':'Graphic Design',
+						'url':'trunkys.graphic-design'
+					},
+					{
+						'label':'UI Design',
+						'url':'trunkys.ui-design'
+					}
+				],
+				'prev':'#/peter-ashmore',
+				'next':'#/footerreveal'
+			},
+			{
+				'name':'Footer-Reveal.js', 
+				'items':[
+					{
+						'label':'Overview',
+						'url':'footerreveal.overview'
+					}, 
+					{
+						'label':'Web Development',
+						'url':'footerreveal.web-development'
+					}
+				],
+				'prev':'#/trunkys',
+				'next':'#/simplespin'
+			},
+			{
+				'name':'Simplespin.css', 
+				'items':[
+					{
+						'label':'Overview',
+						'url':'simplespin.overview'
+					}, 
+					{
+						'label':'Web Development',
+						'url':'simplespin.web-development'
+					}
+				],
+				'prev':'#/footerreveal',
+				'next':''
+			}
     ];
   });
