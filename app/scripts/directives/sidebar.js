07c29a44ad133ad4f48fb58e8d3866d5fefad574
Iain Andrew <iainbruceandrew@gmail.com>
2015-01-03 15:52:37 +0000

    fixed project load animation - no longer fires on every state change

--- app/scripts/directives/sidebar.js
+++ app/scripts/directives/sidebar.js
@@ -8,6 +8,18 @@ angular.module('app.directives.sidebar', [])
 				projects: '=',
 				filterBy: '='
 			},
+			link: function(scope, element, attributes) {
+				var $container = $('.view-container');
+
+				function projectLoad() {
+					if ($('.container').is('.plugmedia-overview, .plugmedia-web-design, .plugmedia-web-development,.plugmedia-logo-design,.peter-ashmore-overview,.peter-ashmore-web-design,.peter-ashmore-web-development,.peter-ashmore-graphic-design,.peter-ashmore-logo-design,.trunkys-overview,.trunkys-graphic-design,.trunkys-ui-design,.footerreveal-overview,.footerreveal-web-development,.simplespin-overview,.simplespin-web-development')) {
+						$container.addClass('project-load');
+					} else {
+						$container.removeClass('project-load');
+					}
+				}
+				setInterval(projectLoad, 100);
+			},
 			templateUrl:'views/sidebar.html'
 		};
 	});
