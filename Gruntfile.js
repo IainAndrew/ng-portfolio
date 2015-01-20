0c3d609729ac6d297670b063ef30a7bdb6afe3d1
Iain Andrew <iainbruceandrew@gmail.com>
2015-01-20 20:44:48 +0000

    added hr below each project section tite

--- Gruntfile.js
+++ Gruntfile.js
@@ -413,7 +413,7 @@ module.exports = function (grunt) {
     // Test settings
     karma: {
       unit: {
-        configFile: 'test/karma.conf.js',
+        configFile: 'karma.conf.js',
         singleRun: true
       }
     },
