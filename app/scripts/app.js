983423c8e6ad4e5298f2e6b1ebbd73570b5bdabb
Iain Andrew <iainbruceandrew@gmail.com>
2015-01-21 15:31:07 +0000

    added binding to <title> tag and title property to each state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,17 +28,26 @@ angular
       .state('about', {
         url: '/',
         templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+        controller: 'AboutCtrl',
+        title: 'About'
       })
       .state('about-more', {
         url: '/about-more',
         templateUrl: 'views/about-more.html',
-        controller: 'AboutCtrl'
+        controller: 'AboutCtrl',
+        title: 'More About Me'
       })
       .state('portfolio', {
         url: '/portfolio',
         templateUrl: 'views/portfolio.html',
-        controller: 'PortfolioCtrl'
+        controller: 'PortfolioCtrl',
+        title: 'Portfolio'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        title: 'Contact'
       })
 
       // Plugmedia
@@ -51,22 +60,26 @@ angular
       .state('plugmedia.overview', {
         url: '/',
         templateUrl: 'views/plugmedia/overview.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Plugmedia'
       })
       .state('plugmedia.web-design', {
         url: '/web-design',
         templateUrl: 'views/plugmedia/webdesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Plugmedia - Web Design'
       })
       .state('plugmedia.web-development', {
         url: '/web-development',
         templateUrl: 'views/plugmedia/webdev.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Plugmedia - Web Development'
       })
       .state('plugmedia.logo-design', {
         url: '/logo-design',
         templateUrl: 'views/plugmedia/logodesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Plugmedia - Logo Design'
       })
 
       // Peter Ashmore
@@ -79,27 +92,32 @@ angular
       .state('peter-ashmore.overview', {
         url: '/',
         templateUrl: 'views/peter-ashmore/overview.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Peter Ashmore'
       })
       .state('peter-ashmore.web-design', {
         url: '/web-design',
         templateUrl: 'views/peter-ashmore/webdesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Peter Ashmore - Web Design'
       })
       .state('peter-ashmore.web-development', {
         url: '/web-development',
         templateUrl: 'views/peter-ashmore/webdev.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Peter Ashmore - Web Development'
       })
       .state('peter-ashmore.graphic-design', {
         url: '/graphic-design',
         templateUrl: 'views/peter-ashmore/graphicdesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Peter Ashmore - Graphic Design'
       })
       .state('peter-ashmore.logo-design', {
         url: '/logo-design',
         templateUrl: 'views/peter-ashmore/logodesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Peter Ashmore - Logo Design'
       })
 
       // Trunkys
@@ -112,17 +130,20 @@ angular
       .state('trunkys.overview', {
         url: '/',
         templateUrl: 'views/trunkys/overview.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Trunkys'
       })
       .state('trunkys.graphic-design', {
         url: '/graphic-design',
         templateUrl: 'views/trunkys/graphicdesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Trunkys - Graphic Design'
       })
       .state('trunkys.ui-design', {
         url: '/ui-design',
         templateUrl: 'views/trunkys/uidesign.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Trunkys - UI Design'
       })
 
       // Footer-reveal.js
@@ -135,12 +156,14 @@ angular
       .state('footerreveal.overview', {
         url: '/',
         templateUrl: 'views/footerreveal/overview.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Footer-reveal.js'
       })
       .state('footerreveal.web-development', {
         url: '/web-development',
         templateUrl: 'views/footerreveal/webdev.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Footer-reveal.js - Web Development'
       })
 
       // Simplespin.css
@@ -153,20 +176,17 @@ angular
       .state('simplespin.overview', {
         url: '/',
         templateUrl: 'views/simplespin/overview.html',
-        controller: 'ProjectCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Simplespin.css'
       })
       .state('simplespin.web-development', {
         url: '/web-development',
         templateUrl: 'views/simplespin/webdev.html',
-        controller: 'ProjectCtrl'
-      })
-
-      .state('contact', {
-        url: '/contact',
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
+        controller: 'ProjectCtrl',
+        title: 'Simplespin.css - Web Development'
       });
   }])
-  .run(function($rootScope, $state) {
+  .run([ '$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;
-  });
+    $rootScope.$stateParams = $stateParams;
+  }])
