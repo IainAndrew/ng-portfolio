99521ae46507288e26280d5de4616f9c38986bb4
Iain Andrew <iainbruceandrew@gmail.com>
2015-01-25 22:28:23 +0000

    added scroll event to main nav interaction

--- app/scripts/directives/logo.js
+++ app/scripts/directives/logo.js
@@ -17,18 +17,37 @@ angular.module('app.directives.logo', [])
 					if (!Modernizr.touch) {
 						$logo.mouseenter(function() {
 							$('nav').addClass('nav-show');
+							$logo.removeClass('logo-scroll');
 						});
 						$('nav').mouseenter(function() {
 							$('nav').addClass('nav-show');
+							$logo.removeClass('logo-scroll');
 						});
 						$('nav').mouseleave(function() {
-							$('nav').removeClass('nav-show');
+							if ( $(window).scrollTop() <= 50 ) {
+								$('nav').addClass('nav-show');
+								$logo.removeClass('logo-scroll');
+							} else {
+								$('nav').removeClass('nav-show');
+								$logo.addClass('logo-scroll');
+							}
 						});	
 					} else {
 						$logo.click(function() {
 							$('nav').toggleClass('nav-show');
+							$logo.removeClass('logo-scroll');
 						});
 					}
+
+					$(window).on('scroll load', function() {
+						if ( $(this).scrollTop() <= 50 ) {
+							$('nav').addClass('nav-show');
+							$logo.removeClass('logo-scroll');
+						} else {
+							$('nav').removeClass('nav-show');
+							$logo.addClass('logo-scroll');
+						}
+					});
 				}, 4500);
 			}
 		};
